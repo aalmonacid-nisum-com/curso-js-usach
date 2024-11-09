afb1e65d236345ef85001711faa548e04624be82
Alex Almonacid <aalmonacid@NTB-217.local>
2024-11-09 18:34:14 -0300

    funcion para sumar modificada

--- tarea-5/js/index.js
+++ tarea-5/js/index.js
@@ -2,12 +2,14 @@
 const numeros = [6, 8, 9, 2, 5, 10];
 
 // creo variable suma
-let suma = 0;
+let suma = 6 + 8 + 9 + 2 + 5 + 10;
 
-// uso de for para realizar la suma
+/* uso de for para realizar la suma
+let suma = 0;
 for (let i = 0; i < numeros.length; i++) {
   suma += numeros[i];
 }
+*/
 
 // calcular el promedio
 const promedio = suma / numeros.length;
